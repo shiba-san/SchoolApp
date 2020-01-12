880b575770d1b1b8afa8ca64a67328720aa7bfc4
Hong Jun <howho8039h@tmjc.edu.sg>
2020-01-12 20:04:09 +0800

    first change installed navigation bottom

--- components/navigation/SignUp.js
+++ components/navigation/SignUp.js
@@ -1,26 +1,8 @@
-import React, { Component } from 'react';
-import { 
-  StyleSheet,
-  Text,
-  View 
-} from 'react-native';
-
+import React from 'react';
+import Navigator from './SignUpFolder/signupStack';
 
 export default function SignUp() {
-
-    return (
-    <View style={styles.container}>
-      <Text>SignUp</Text>
-    </View>
-
-    )
+  return (
+    <Navigator/>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#cadefc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
