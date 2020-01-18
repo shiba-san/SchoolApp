4a7f7673dafcbc86ff9d1ace3332a114a702f197
shiba-san <51448644+shiba-san@users.noreply.github.com>
2020-01-18 09:37:03 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Navigator from './LoginStack';
+import Amplify from 'aws-amplify';
+import amplify from './YOUR-PATH-TO/aws-exports';
+
+Amplify.configure(amplify);
 
 export default function App() {
   return (
